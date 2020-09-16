6f5b469fe3f8df3a5d83408b8da43a27fdcf2da8
boom-roasted <noah.prezant@gmail.com>
2020-09-16 01:57:01 +0000

    add cache module

---
+++ src/bbd/cache/__init__.py
@@ -0,0 +1,3 @@
+from .cache import cache_, set_working_directory
+
+__all__ = [cache_, set_working_directory]
--- src/bbd/census/cache.py
+++
@@ -1,6 +0,0 @@
-def get_cached_file(path):
-    pass
-
-
-def cache_file(path):
-    pass
