d63343b2d66e663baaaafecae1c10ffb62c53545
boom-roasted <noah.prezant@gmail.com>
2020-09-19 00:54:41 +0000

    setup trim_shapefile method

---
+++ src/bbd/census/trim_shapefile.py
@@ -0,0 +1,9 @@
+"""
+TODO this kind of belongs here because it will be used with get_shapefile,
+but it should be moved to it's own utility module if any other code outside
+of the census module wants to use it.
+"""
+
+
+def trim_shapefile():
+    pass
