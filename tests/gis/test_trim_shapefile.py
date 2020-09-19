b50a9a6b8bc5b791b2ccab76da7fb85e7f83e42c
boom-roasted <noah.prezant@gmail.com>
2020-09-19 04:33:08 +0000

    add a trim_shapefile test

---
+++ tests/gis/test_trim_shapefile.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import shapefile
+
+from bbd import census
+
+here = Path(__file__).parent
+
+
+def test_trim_shapefile():
+    in_path = here / "shapefiles/co/tl_2019_08_cd116"
+    join_on = "GEOID"
+    include = [
+        "0804",
+        "0803",
+        "0802",
+        "0801",
+    ]
+
+    out_path = census.trim_shapefile(in_path, join_on, include)
+
+    with shapefile.Reader(str(out_path)) as r:
+
+        shape_records = r.shapeRecords()
+        assert len(shape_records) == 4
+
+        geoids = [sr.record["GEOID"] for sr in shape_records]
+        assert sorted(geoids) == sorted(include)
