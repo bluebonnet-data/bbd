1c8429bcd5d0ed4aef7bee2a4574510b664f0021
boom-roasted <noah.prezant@gmail.com>
2020-09-20 00:45:57 +0000

    remove addressed TODO regarding location of trim_shapefile.py

--- src/bbd/gis/trim_shapefile.py
+++ src/bbd/gis/trim_shapefile.py
@@ -1,9 +1,3 @@
-"""
-TODO this kind of belongs here because it will be used with get_shapefile,
-but it should be moved to it's own utility module if any other code outside
-of the census module wants to use it.
-"""
-
 import shutil
 from pathlib import Path
 from typing import Union
