56e24d3a0a2af4ca71b02dea881e6bc9e08d6380
boom-roasted <noah.prezant@gmail.com>
2020-10-02 23:39:10 +0000

    census json loader now returns all available data by default

--- src/bbd/census/load.py
+++ src/bbd/census/load.py
@@ -1,7 +1,7 @@
 import json
 
 
-def load_json_file(fp, headers: list) -> dict:
+def load_json_file(fp, headers: list = None) -> dict:
     """Extract column data for the requested headers"""
 
     with open(fp, "r") as f:
@@ -10,21 +10,25 @@ def load_json_file(fp, headers: list) -> dict:
     return organize(data, headers)
 
 
-def load_json_str(s: str, headers: list) -> dict:
+def load_json_str(s: str, headers: list = None) -> dict:
     """Extract column data for the requested headers"""
 
     data = json.loads(s)
     return organize(data, headers)
 
 
-def organize(data: dict, headers: list) -> dict:
+def organize(data: dict, headers: list = None) -> dict:
     """Extract column data for the requested headers"""
 
     # Top row is header row
     all_headers = data[0]
 
-    # Get indexes for each requested header
-    indexes = [all_headers.index(h) for h in headers]
+    # Get indexes for each requested header. If there are no requested headers, get
+    # all of the data available.
+    if headers is None:
+        indexes = [i for i in range(all_headers)]
+    else:
+        indexes = [all_headers.index(h) for h in headers]
 
     # Construct data container
     d = {h: [] for h in headers}
