36f7c61e2481f35db76188f6f8345dfad153dd65
DarkSideOfTheMat <48082583+DarkSideOfTheMat@users.noreply.github.com>
2020-10-21 23:09:26 -0700

    Update __init__.py
    changed name of GeocodeLocation to GeocodeLocations

--- src/bbd/geocoder/__init__.py
+++ src/bbd/geocoder/__init__.py
@@ -1,11 +1,11 @@
 from .geocoder import (get_geocoder, 
                        get_reverse_geocoder,
                        street_encode,
-                       GeocodeLocation)
+                       GeocodeLocations)
 
 __all__ = [
 	get_geocoder,
 	get_reverse_geocoder,
 	street_encode,
-	GeocodeLocation
+	GeocodeLocations
 ]
